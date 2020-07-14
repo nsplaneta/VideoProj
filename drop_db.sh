aaec8945e3213dc39620aeb5c7396f95e2758d26
Nicolo Planeta <nico@Air-di-Nicolo.home>
2020-07-14 21:24:01 +0200

    Owner

--- drop_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "rebuilding database ..."
-php bin/console doctrine:schema:drop -n -q --force --full-database
-rm migrations/*.php
-php bin/console make:migration
-php bin/console doctrine:migrations:migrate -n -q
-php bin/console doctrine:fixtures:load -n -q
-
